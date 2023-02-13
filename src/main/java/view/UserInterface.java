92c5a06c600aa36eb53c786b043b7fef004df4b7
Mohammadmahdi <fmohammadmahdi1381@gmail.com>
2023-02-13 18:45:56 +0330

    userinterface constants extracted

--- src/main/java/view/UserInterface.java
+++ src/main/java/view/UserInterface.java
@@ -10,17 +10,18 @@ import model.query.ComplexQueryDecoder;
 import java.util.*;
 
 public class UserInterface {
-
+    private final String END = "end";
+    private final String ENTER_PHRASE = "Enter phrase(\"end\" to finish the program):";
     public void run(InvertedIndex invertedIndex) {
-        final String enterPhrase = "Enter phrase(\"end\" to finish the program):";
         Scanner myScanner = new Scanner(System.in);
-        System.out.println(enterPhrase);
+        System.out.println(ENTER_PHRASE);
         String input = myScanner.nextLine();
-        while (!input.equals("end")) {
+
+        while (!input.equals(END)) {
             ComplexQuery query = new ComplexQuery(input, invertedIndex, new ComplexQueryDecoder());
             for (String s : query.sendQueryResponse())
                 System.out.print(s + "\t");
-            System.out.println(enterPhrase);
+            System.out.println(ENTER_PHRASE);
             input = myScanner.nextLine();
         }
         myScanner.close();
